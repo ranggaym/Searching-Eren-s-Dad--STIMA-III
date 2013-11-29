2d19892605d3378f4846a96a5f1d9f95f551b3c1
Rangga Yustian <rangga_yustian@yahoo.com>
2013-11-30 06:31:57 +0700

    KMP

---
+++ Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringpatternmatching;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author RANGGA Y.MANGINDAAN
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        String t = "aaaAbaaaaAbaaaAbaaAbaaAb";
+        String p = "Ab";
+
+        KMP kmp = new KMP (t, p);
+        System.out.println("Text: " + t);
+        System.out.println("Pattern: " + p);
+
+        int posn = kmp.kmpMatch(t, p);
+        if (posn == -1)
+          System.out.println("Pattern not found");
+        else 
+          System.out.println("Pattern starts at posn " 
+                                            + posn);
+        ArrayList<Integer> r2 = new ArrayList<>();
+        r2 = kmp.matchAllPatternKMP(t, p);
+        
+        for (int x : r2){
+            System.out.println("Pattern starts at posn " + x);
+        }
+    
+    }
+    
+    
+    
+}
